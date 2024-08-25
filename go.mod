d6495b3b376872c0b10bf9a78b79cb8a0e985882
guanren <so.guanren@gmail.com>
2024-08-25 13:37:18 +0800

    优化

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module proxyja3
 
-go 1.22.4
+go 1.22.6
 
 require (
 	github.com/qtgolang/SunnyNet v1.0.2
@@ -8,8 +8,8 @@ require (
 )
 
 require (
-	github.com/Trisia/gosysproxy v1.0.0 // indirect
-	golang.org/x/crypto v0.12.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
+	github.com/Trisia/gosysproxy v1.1.0 // indirect
+	golang.org/x/crypto v0.26.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,48 +1,69 @@
 package main
 
 import (
+	"log"
+
 	"github.com/qtgolang/SunnyNet/SunnyNet"
 	"github.com/qtgolang/SunnyNet/public"
 	"github.com/spf13/pflag"
-	"log"
 )
 
-var Sunny = SunnyNet.NewSunny()
-var Port int
+var (
+	Sunny = SunnyNet.NewSunny()
+	Port  int
+)
 
 func main() {
-
+	// 解析命令行参数，设置代理端口
 	pflag.IntVarP(&Port, "port", "p", 18080, "代理端口")
 	pflag.Parse()
-	log.Println("代理端口:", Port)
+
+	log.Printf("代理端口: %d\n", Port)
+
+	// 设置回调函数
 	Sunny.SetGoCallback(HttpCallback, TcpCallback, WSCallback, UdpCallback)
-	//绑定端口号并启动
+
+	// 绑定端口号并启动服务
 	s := Sunny.SetPort(Port)
 	s.SetRandomTLS(true)
-	s.Start()
+
+	if err := s.Start(); err != nil {
+		log.Fatalf("服务启动失败: %v", err)
+	}
 	log.Println("服务启动成功")
+
+	// 阻止程序退出
 	select {}
 }
+
+// HttpCallback 处理 HTTP 请求的回调函数
 func HttpCallback(Conn *SunnyNet.HttpConn) {
-	log.Printf("HttpCallback:请求方法:%v-请求ip:%v-请求地址:%v\n", Conn.Request.Method, Conn.ClientIP, Conn.Request.URL)
+	log.Printf("HttpCallback: 请求方法: %v, 请求 IP: %v, 请求地址: %v\n", Conn.Request.Method, Conn.ClientIP, Conn.Request.URL)
 }
+
+// TcpCallback 处理 TCP 请求的回调函数
 func TcpCallback(Conn *SunnyNet.TcpConn) {
-	//捕获到数据可以修改,修改空数据,取消发送/接收
-	//Conn.SetAgent("") //这个是针对这个一个请求的,TCP,只能设置S5代理,
-	//fmt.Println(Conn.Pid, Conn.LocalAddr, Conn.RemoteAddr, Conn.Type, Conn.GetBodyLen())
+	// 捕获到数据可以修改, 修改空数据, 取消发送/接收
+	// Conn.SetAgent("") // 这个是针对这个一个请求的, TCP, 只能设置 S5 代理
+	// log.Printf("TCP: PID: %v, LocalAddr: %v, RemoteAddr: %v, Type: %v, BodyLen: %v\n", Conn.Pid, Conn.LocalAddr, Conn.RemoteAddr, Conn.Type, Conn.GetBodyLen())
 }
-func WSCallback(Conn *SunnyNet.WsConn) {}
+
+// WSCallback 处理 WebSocket 请求的回调函数
+func WSCallback(Conn *SunnyNet.WsConn) {
+	// WebSocket 回调逻辑
+}
+
+// UdpCallback 处理 UDP 请求的回调函数
 func UdpCallback(Conn *SunnyNet.UDPConn) {
-	//在 Windows 捕获UDP需要加载驱动,并且设置进程名
-	//其他情况需要设置Socket5代理,才能捕获到UDP
-	//捕获到数据可以修改,修改空数据,取消发送/接收
-	if public.SunnyNetUDPTypeReceive == Conn.Type {
-		//fmt.Println("接收UDP", Conn.LocalAddress, Conn.RemoteAddress, len(Conn.Data))
-	}
-	if public.SunnyNetUDPTypeSend == Conn.Type {
-		//fmt.Println("发送UDP", Conn.LocalAddress, Conn.RemoteAddress, len(Conn.Data))
-	}
-	if public.SunnyNetUDPTypeClosed == Conn.Type {
-		//fmt.Println("关闭UDP", Conn.LocalAddress, Conn.RemoteAddress)
+	// 在 Windows 捕获 UDP 需要加载驱动, 并且设置进程名
+	// 其他情况需要设置 Socket5 代理, 才能捕获到 UDP
+	// 捕获到数据可以修改, 修改空数据, 取消发送/接收
+	switch Conn.Type {
+	case public.SunnyNetUDPTypeReceive:
+		// log.Printf("接收 UDP: LocalAddress: %v, RemoteAddress: %v, DataLen: %d\n", Conn.LocalAddress, Conn.RemoteAddress, len(Conn.Data))
+	case public.SunnyNetUDPTypeSend:
+		// log.Printf("发送 UDP: LocalAddress: %v, RemoteAddress: %v, DataLen: %d\n", Conn.LocalAddress, Conn.RemoteAddress, len(Conn.Data))
+	case public.SunnyNetUDPTypeClosed:
+		// log.Printf("关闭 UDP: LocalAddress: %v, RemoteAddress: %v\n", Conn.LocalAddress, Conn.RemoteAddress)
 	}
 }
